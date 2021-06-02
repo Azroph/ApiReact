37a8673c6b869c6379df5b63f3e37645015bfe39
Azroph <56892575+Azroph@users.noreply.github.com>
2021-06-02 15:51:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,60 +8,59 @@ class App extends React.Component {
     items: []
   }
 
-  componentDidMount(){
-    console.log('componentDidMount');
+  componentDidMount() {
+    console.log("componentDidMount");
 
-    fetch('https://dekpo.herokuapp.com/posts/')
-    .then(response => response.json())
-    .then(
-      data => {
-        this.setState({
-          isLoaded: true,
-          items: data
-        })
-        console.log( 'Fetch success', data)
-      },
-      error => {
-        this.setState({
-          error: error
-        })
-        console.log(error)
-      }
+    fetch('https://dekpo.herokuapp.com/posts')
+      .then(response => response.json())
+      .then(
+        data => {
+          this.setState({
+            isLoaded: true,
+            items: data
+          })
+          console.log('Fetch success', data)
+        },
+        error => {
+          this.setState({
+            error: error
+          })
+          console.log(error)
+        }
       )
   }
 
-  render(){
+  render() {
     const { error, isLoaded, items } = this.state;
 
-    if (error){
-      return(
-        <div> ERROR</div>
+    if (error) {
+      return (
+        <div>Oups ERROR</div>
       )
     } else if (!isLoaded) {
-      return(
-        <div> Chargement en cours </div>
+      return (
+        <div>Chargement toujours en cours...</div>
       )
     } else {
-      return(
-        items.map( item => (
+      return (
+        items.map(item => (
           <article key={item._id}>
             {
-              (() => {
-                if (item.img) {
-                  return(
-                    <img src={"https://dekpo.herokuapp.com/posts/" + item._img} alt={item.title} />
-                  )
-                }
-              })()
+            (() => {
+              if (item.img) {
+                return(
+                  <img src={"https://dekpo.herokuapp.com/posts/" + item.img} alt={item.title} />
+                )
+              }
+            })()
             }
-            <h2>{item.title}</h2>
-            <p>{item.text}</p>
+          <h2>{item.title}</h2>
+          <p>{item.text}</p>
           </article>
         ))
       )
     }
   }
-
 }
 
 export default App;
